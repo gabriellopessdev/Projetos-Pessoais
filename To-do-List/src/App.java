32e4d066c8b70b76c6d8b1db6ea1bee2d6c9f550
Tibiao <gabrielantoniosantoslopes.com>
2024-05-24 15:22:35 -0300

    Adicionando Tarefas

--- To-do-List/src/App.java
+++ To-do-List/src/App.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        List<String> tarefas = new ArrayList<>();
+
+        System.out.println("Adicionar Tarefa:");
+        String novaTarefa = scanner.nextLine();
+        tarefas.add(novaTarefa);
+        System.out.println("Tarefa Adicionada:" + novaTarefa);
+
+
+
+        
+        scanner.close();
     }
 }
